45ee3dacebf14b674e31a6a7ac541ac6546355f8
willyhuang <aa0981273968@gmail.com>
2026-03-18 12:50:30 +0800

    1st update

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ def stock_info(id):
     print(f'今年度EPS {info.get("priceEpsCurrentYear")}')
     print(f'今日行情 {info.get("regularMarketDayRange")}')
 
+def ticker_summary(id):
+    tick = yf.Ticker(id)
+    info = tick.fast_info
+    print(f'開盤價{info.get("open")}')
+    print(f'今日最高價{info.get("dayHigh")}')
+    print(f'今日最低價{info.get("dayLow")}')
+    print(f'目前價格{info.get("last_price")}')
+    print(f'成交量{info.get("last_volume")}')     
+          
 
-stock_info('2330.TW')
-stock_info('3533.TW')
+
+#stock_info('2330.TW')
+#stock_info('3533.TW')
+print('------------台積電------------')
+ticker_summary('2330.TW')
+print('------------嘉澤------------')
+ticker_summary('3533.TW')
